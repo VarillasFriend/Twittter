9867a549886675422aa5a06dddf4f71331e865a2
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2020-05-24 19:57:47 -0300

    1st part

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,4 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
 
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+# config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
